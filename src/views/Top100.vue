172f9371e226b2a43c812bd3214df7f41b9e2475
Sdyess <dyesssp@gmail.com>
2019-05-12 00:08:28 -0500

    Top 100 should use the non-filtered streams

--- src/views/Top100.vue
+++ src/views/Top100.vue
@@ -24,7 +24,7 @@ export default {
       return this.$store.state.streamsLoaded;
     },
     topStreams() {
-      return this.$store.state.streams;
+      return this.$store.state.topStreams;
     },
   },
   created() {
